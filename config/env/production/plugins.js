2775cee9dbe49f384746d58b00c064f2f0374213
thalyscml <thalys.correia@netimoveis.com>
2023-06-22 10:40:59 -0300

    add provider upload plugin

---
+++ config/env/production/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-azure-storage",
+      providerOptions: {
+        account: env("STORAGE_ACCOUNT"),
+        accountKey: env("STORAGE_ACCOUNT_KEY"),
+        serviceBaseURL: env("STORAGE_URL"), // optional
+        containerName: env("STORAGE_CONTAINER_NAME"),
+        defaultPath: "assets",
+      },
+    },
+  },
+});
