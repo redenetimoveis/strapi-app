8c32c481614466be7431354f73c215c84f534c55
thalyscml <thalys.correia@netimoveis.com>
2023-07-06 15:33:41 -0300

    development test

--- config/server.js
+++ config/server.js
@@ -1,4 +1,4 @@
-/* module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
@@ -6,13 +6,5 @@
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
- */
-module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('APP_URL'), // Sets the public URL of the application.
-  app: { 
-    keys: env.array('APP_KEYS')
   },
 });
